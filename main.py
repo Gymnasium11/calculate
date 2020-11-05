37792b85ed52351a8a1ae159156d5613bbb6820e
NikSuperPlay <NikSuperPlay123@rambler.ru>
2020-11-05 21:04:31 +0700

    Half of completed task

--- main.py
+++ main.py
@@ -5,53 +5,59 @@ def main(level):
     oper = ["+", "-", "*", "/"]
     rndoper = random.choice(oper)
 
-    # def sum(x,y):
-    # def mult(x,y):
-    # def sub(x,y):
-    # def div(x,y):
-
+    def sum(x,y):
+        res=x+y
+        return res
+    def mult(x,y):
+        res=x*y
+        return res
+    def sub(x,y):
+        """Не понимаю как сделать так,
+         чтобы был неотрицательный результат"""
+    def div(x,y):
+        res=x/y
+        return res
     if level == "1":
-        if rndoper in '+-':  # объявляю x и y если будут действия бычитания или сложения
+        if rndoper in '+-':  # объявляю x и y, если будут действия вычитания или сложения
             x, y = random.randint(1, 50), random.randint(1, 50)
-        else:  # объявляю x и y если будут действия умножения или деления
+        else:  # объявляю x и y, если будут действия умножения или деления
             x, y = random.randint(1, 10), random.randint(1, 10)
 
         if rndoper == "+":
-            pass
+            return sum(x,y)
         elif rndoper == "-":
-            pass
+            return sub(x,y)
         elif rndoper == "*":
-            pass
+            return mult(x,y)
         elif rndoper == "/":
-            """То же самое, как с вычитанием"""
-            pass
+            return div(x,y)
 
     elif level == "2":
-        if rndoper in '+-':  # объявляю x и y если будут действия бычитания или сложения
+        if rndoper in '+-':  # объявляю x и y, если будут действия вычитания или сложения
             x, y = random.randint(1, 100), random.randint(1, 100)
-        else:  # объявляю x и y если будут действия умножения или деления
+        else:  # объявляю x и y, если будут действия умножения или деления
             x, y = random.randint(1, 30), random.randint(1, 30)
 
         if rndoper == "+":
-            pass
+            return sum(x, y)
         elif rndoper == "-":
-            pass
+            return sub(x,y)
         elif rndoper == "*":
-            pass
+            return mult(x,y)
         elif rndoper == "/":
-            pass
+            return div(x,y)
 
     elif level == "3":
-        if rndoper in '+-':  # объявляю x и y если будут действия бычитания или сложения
+        if rndoper in '+-':  # объявляю x и y, если будут действия вычитания или сложения
             x, y = random.randint(1, 100), random.randint(1, 100)
-        else:  # объявляю x и y если будут действия умножения или деления
+        else:  # объявляю x и y, если будут действия умножения или деления
             x, y = random.randint(1, 50), random.randint(1, 50)
 
         if rndoper == "+":
-            pass
+            return sum(x,y)
         elif rndoper == "-":
-            pass
+            return sub(x,y)
         elif rndoper == "*":
-            pass
+            return mult(x,y)
         elif rndoper == "/":
-            pass
+            return div(x,y)
